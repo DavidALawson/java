ec675b85ca9c136b4e318c34bfc26ceeec984f20
DavidALawson <dlawson27@gmail.com>
2017-03-23 18:52:11 -0400

    Fleshed out the Cache interface with JavaDoc comments and the following methods: get(K), put(K,V) contains(K), flush(), getCapacity(), getNumberStored(), and isEmpty().

--- data-structures/src/cache/Cache.java
+++ data-structures/src/cache/Cache.java
@@ -21,12 +21,75 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package cache;
 
 /**
- *
+ * <p>
+ * An object that stores other objects for future use.
+ * </p>
+ * <p>
+ * Implementations of Cache will have to determine how they will insert
+ * objects when the capacity is reached. Possible implementations include random 
+ * replacement (RR), least recently used (LRU), and most recently used (MRU).
+ * </p>
  * @author DavidALawson
+ * @param <K> represents the type of key that will be used to retrieve the object
+ * stored in the Cache.
+ * @param <V> represents the type of the object stored in the Cache.
  */
-public interface Cache {
+public interface Cache<K,V> {
     
+    /**
+     * Gets the object from the Cache that corresponds to the key passed in.
+     * @param key the key that is linked to the object
+     * @return the object the key is linked to, or null if no object is
+     * linked to the key
+     */
+    V get(K key);
+
+    /**
+     * Puts the value in the Cache at the given key. If the Cache is full, then
+     * a value in the Cache will be replaced. The replaced value is returned.
+     * Implementors will determine the way in which the value to
+     * replace is chosen.
+     * @param key the key to link the object to
+     * @param value the value to add to the Cache
+     * @return the value that this replaces, or null if no object was replaced
+     */
+    V put(K key, V value);
+    
+    /**
+     * Returns true if the key links to a stored object in the Cache.
+     * @param key the key to check if an object is stored under
+     * @return true if an object is stored under this key, false if there is not
+     * an object stored under this key
+     */
+    boolean contains(K key);
+    
+    /**
+     * Flushes the Cache of all stored objects. Returns the Cache to its initial
+     * state of empty with the same capacity as before the flush occurred.
+     */
+    void flush();
+    
+    /**
+     * Returns the capacity of the Cache. This is the maximum number of objects
+     * the Cache can contain.
+     * @return the capacity of the Cache
+     */
+    int getCapacity();
+    
+    /**
+     * Returns the number of objects stored in the Cache. This is the number of
+     * objects the Cache has stored within.
+     * @return the number of objects stored in the Cache
+     */
+    int getNumberStored();
+    
+    /**
+     * Returns true if the Cache has no stored objects.
+     * @return true if the cache is empty, false if it is not
+     */
+    boolean isEmpty();
 }
